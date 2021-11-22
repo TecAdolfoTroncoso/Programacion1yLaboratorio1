792c168998a204af403d21500cd313240b7aa1da
TecAdolfoTroncoso <tec.troncosoadolfo@outlook.com.ar>
2021-11-22 16:47:32 -0300

    Update

--- Ejercicios Guia 3/Array/Eje09_guia3.c
+++ Ejercicios Guia 3/Array/Eje09_guia3.c
@@ -8,40 +8,54 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
+#define MAX 5
 
 int cargar(int numero[]);
-int mostrar(int numero[]);
-
-int main(){
-  system("cls");
-  system ("color 70");
-  int numeros[MAX];
-  cargar(numeros);
-  mostrar(numeros);
-
+int multiplicar(int numero2[], int numero[]);
+int mostrar(int numero2[]);
+int main()
+{
+    system("cls");
+    system("color 70");
+    int numeros[MAX];
+    int numeros2[MAX];
+    cargar(numeros);
+    multiplicar(numeros,numeros2);
+    mostrar(numeros2);
 }
 
-
-int cargar(int numero[]){
-  int i,lim;
-  for(lim=0;lim<=10;lim++){ //limpio el array para evitar suciedad dentro.
-    numero[lim]=0;
-  }
-  for ( i = 0; i < MAX; i++)
-  {
-    printf("Numero de vuelta '%d'  Ingrese un numero porfavor:\n",i+1);
-    scanf("%d",&numero[i]);
-  }
-  
+int cargar(int array1[])
+{
+    int i, lim;
+    for (lim = 0; lim < MAX; lim++)
+    { //limpio el array para evitar suciedad dentro.
+        array1[lim] = 0;
+    }
+    for (i = 0; i < MAX; i++)
+    {
+        printf("Numero de vuelta '%d'  Ingrese un numero porfavor:\n", i + 1);
+        scanf("%d", &array1[i]);
+    }
 }
 
-int mostrar (int numero[]){
-  int i;
-  for(i=0;i<10;i++){ 
+int multiplicar(int array1[], int array2[])
+{
+    int i, lim;
+    for (lim = 0; lim < MAX; lim++)
+    { //limpio el array para evitar suciedad dentro.
+        array2[lim] = 0;
+    }
 
-  printf("%d\t",numero[i]);
-  }
+    for (i = 0; i < MAX; i++)
+    {
+        array2[i] = array1[i] * 2;
+    }
+     
+}
 
-  }
-  
+int mostrar(int muestra[]){
+    for(int i=0;i<MAX;i++){
+        printf("%d\t",muestra[i]);
+    }
+}
+
